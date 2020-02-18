170f36f90a854e1016c7bd866461266e4067e73e
Kenneth K. Slachta, Jr <ken.slachta@gmail.com>
2020-02-18 12:53:26 -0700

    exported core-data

--- libs/core-data/src/index.ts
+++ libs/core-data/src/index.ts
@@ -1,1 +1,3 @@
-export * from './lib/core-data.module';
+export { CoreDataModule } from './lib/core-data.module';
+export { Drinks } from './lib/energy-drinks/energy-drinks';
+export { EnergyDrinksService } from './lib/energy-drinks/energy-drinks.service';
